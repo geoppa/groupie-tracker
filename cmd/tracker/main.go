931716deeaa2e9903dea747022d32a8900ccc336
elgeorgiou <1Eleanaki@gmail.com>
2026-07-12 23:36:54 +0300

    feat(server): serve static assets

--- cmd/tracker/main.go
+++ cmd/tracker/main.go
@@ -17,6 +17,8 @@ func main() {
 		return
 	}
 
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
 	http.HandleFunc("/", handlers.ArtistsHandler)
 
 	fmt.Println("Server starting on http://localhost:8080")
