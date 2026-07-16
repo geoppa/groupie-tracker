6aefca246a8564d23698a941dbd05b07ba7f3777
elgeorgiou <1Eleanaki@gmail.com>
2026-07-16 23:36:48 +0300

    feat(search): initialize search DOM references

---
+++ static/js/search.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// References to the HTML elements used by the search component.
+const searchForm = document.querySelector('.search__form');
+const searchInput = document.getElementById('artist-search');
+const searchSuggestions = document.getElementById('artist-suggestions');
+const searchButton = document.querySelector('.search__submit');
+const clearButton = document.querySelector('.search__clear');
+
